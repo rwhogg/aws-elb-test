de93867f4a0ccb05695ee98c6015990626da0810
Bob Wombat Hogg <wombat@rwhogg.site>
2020-04-18 07:19:54 -0400

    Initial commit

---
+++ src/main/java/com/github/rwhogg/App.java
@@ -0,0 +1,21 @@
+package com.github.rwhogg;
+
+import com.amazonaws.services.elasticloadbalancing.*;
+import com.amazonaws.services.elasticloadbalancing.model.*;
+import java.util.*;
+
+public class App 
+{
+    public static void main(String[] args)
+    {
+        final AmazonElasticLoadBalancingClient elbClient = new AmazonElasticLoadBalancingClient();
+        elbClient.setEndpoint("http://localhost:5000");
+        final Listener listener = new Listener("HTTP", 3000, 4000);
+        final CreateLoadBalancerRequest request = new CreateLoadBalancerRequest("MyLoadBalancer")
+            .withListeners(Arrays.asList(listener));
+        final CreateLoadBalancerResult result = elbClient.createLoadBalancer(request);
+
+        final DescribeLoadBalancersResult describeResult = elbClient.describeLoadBalancers();
+        System.out.println(describeResult.toString());
+    }
+}
